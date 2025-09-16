e3df3f96c40a359b63e8b3b75df6b2e17de1805f
drflorean <florean.daniele@protonmail.com>
2025-09-16 12:43:24 +0200

    added basic data desc and reg to script file

--- Script.R
+++ Script.R
@@ -11,3 +11,21 @@ lapply(libraries, library, character.only=T)
 
 issp_df <- read_dta("issp2009ger.dta")
 View(issp_df)
+names(issp_df)
+
+### description ---------
+table(issp_df$incdiff100)
+
+dtab <- issp_df %>% summarise(
+  mean = mean(incdiff100, na.rm=T),
+  median = median(incdiff100, na.rm=T),
+  min = min(incdiff100, na.rm=T),
+  max = max(incdiff100, na.rm=T))
+View(dtab)
+
+### Linear regression -------------
+issp_reg <- issp_df %>% select(incdiff100, procjust) %>% drop_na()
+
+reg1 <- lm(incdiff100~procjust, data = issp_reg)  ## Y ~ X, data=dataset
+reg1 ## VEEEERY basic output
+summary(reg1) ### Better output but not as good as stata :(
